64f12f371d02c1fd8b6642f66934d8f79e45275e
GubinYuriy <iosdevelop.2022@gmail.com>
2022-11-17 12:03:54 +0500

    Add images

--- myCrypto/Crypto.swift
+++ myCrypto/Crypto.swift
@@ -6,54 +6,54 @@
 //
 
 struct Crypto: Decodable {
-    let status: CoinsStatus
+    //let status: CoinsStatus
     let data: [CoinsData]
 }
 
-struct CoinsStatus: Decodable {
-    let timestamp: String
-}
+//struct CoinsStatus: Decodable {
+//    let timestamp: String
+//}
 
 struct CoinsData: Decodable {
     let name: String
     let symbol: String
     let slug: String
-    let maxSupply: Int
-    let totalSupply: Int
+//    let max_supply: Int
+//    let total_supply: Int
     let quote: Quote
     
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case symbol = "symbol"
-        case slug = "slug"
-        case maxSupply = "max_supply"
-        case totalSupply = "total_supply"
-        case quote = "quote"
-    }
+//    enum CodingKeys: String, CodingKey {
+//        case name = "name"
+//        case symbol = "symbol"
+//        case slug = "slug"
+//        case maxSupply = "max_supply"
+//        case totalSupply = "total_supply"
+//        case quote = "quote"
+//    }
 }
 
 struct Quote: Decodable {
-    let usd: USD
+    let USD: USD
 }
 
 struct USD: Decodable {
     let price: Double
-    let volumePerDay: Double
-    let percentChangeOneHour: Double
-    let percentChangePerDay: Double
-    let percentChangeWeek: Double
-    let percentChangeOneMonth: Double
-    let percentChangeTwoMonths: Double
-    let percentChangeThreeMonths: Double
-    
-    enum CodingKeys: String, CodingKey {
-        case price = "price"
-        case volumePerDay = "volume_24h"
-        case percentChangeOneHour = "percent_change_1h"
-        case percentChangePerDay = "percent_change_24h"
-        case percentChangeWeek = "percent_change_7d"
-        case percentChangeOneMonth = "percent_change_30d"
-        case percentChangeTwoMonths = "percent_change_60d"
-        case percentChangeThreeMonths = "percent_change_90d"
-    }
+    let volume_24h: Double
+    let percent_change_1h: Double
+    let percent_change_24h: Double
+    let percent_change_7d: Double
+    let percent_change_30d: Double
+    let percent_change_60d: Double
+    let percent_change_90d: Double
+
+//    enum CodingKeys: String, CodingKey {
+//        case price = "price"
+//        case volumePerDay = "volume_24h"
+//        case percentChangeOneHour = "percent_change_1h"
+//        case percentChangePerDay = "percent_change_24h"
+//        case percentChangeWeek = "percent_change_7d"
+//        case percentChangeOneMonth = "percent_change_30d"
+//        case percentChangeTwoMonths = "percent_change_60d"
+//        case percentChangeThreeMonths = "percent_change_90d"
+//    }
 }
--- myCrypto/CustomViewCell.swift
+++ myCrypto/CustomViewCell.swift
@@ -19,11 +19,13 @@ class CustomViewCell: UITableViewCell {
     func configure(with coin: CoinsData) {
         coinNameLabel.text = coin.name
         coinSymbolLabel.text = coin.symbol
-        priceLabel.text = String(format: "%.2f", coin.quote.usd.price)
+        priceLabel.text = String(format: "%.2f", coin.quote.USD.price)
         
         DispatchQueue.global().async {
-            let stringUrl = "https://cryptocurrencyliveprices.com/api/\(coin.name.lowercased()).png"
-            guard let imageUrl = URL(string: stringUrl) else { return }
+            let stringUrl = "https://cryptoicons.org/api/icon/\(coin.symbol.lowercased())/50"
+            guard let imageUrl = URL(string: stringUrl) else {
+                print("Error url data")
+                return }
             guard let imageData = try? Data(contentsOf: imageUrl) else { return }
             
             DispatchQueue.main.async {
