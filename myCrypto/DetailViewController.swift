035c6487bb5148068b9f709c8fe464749910061f
GubinYuriy <iosdevelop.2022@gmail.com>
2022-11-18 17:10:36 +0500

    Add outlets

---
+++ myCrypto/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  myCrypto
+//
+//  Created by Юрий Губин on 18.11.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var coinViewImage: UIView!
+    
+    @IBOutlet var priceLabel: UILabel!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var dailyTradingLabel: UILabel!
+    @IBOutlet var totalSupplyLabel: UILabel!
+    @IBOutlet var maxSupplyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    private func setupViews() {
+        
+    }
+
+   
+
+}
